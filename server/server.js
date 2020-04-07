864b2f72eb08b78b5bd7b5c2e26f6633e279e11b
Bruno <bruno.rossetto97@gmail.com>
2020-04-07 20:56:07 -0300

    cors

--- server/server.js
+++ server/server.js
@@ -1,14 +1,15 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const cors = require('cors')
 
-app.listen(8081, function () {
-    console.log(`Listening on 8081`);
-});
+app.use(cors())
+app.use('/static', express.static(path.join(__dirname, '..', 'src')));
+app.use('/static', express.static(path.join(__dirname, '..', 'assets')));
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '..', 'index.html'));
 })
-
-app.use('/static', express.static(path.join(__dirname, '..', 'src')));
-app.use('/static', express.static(path.join(__dirname, '..', 'assets')));
+app.listen(8081, function () {
+    console.log(`Listening on 8081`);
+});
